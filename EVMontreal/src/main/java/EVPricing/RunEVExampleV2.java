4becdc54ae6e194956c190e8e89e54adbbcba8d6
ashrafzaman <ashrafzaman@sansfil-securise-employes-poly-236-171.polymtl.ca>
2022-04-05 18:35:17 -0400

    convenient class to run urbanev example

---
+++ EVMontreal/src/main/java/EVPricing/RunEVExampleV2.java
@@ -0,0 +1,216 @@
+package EVPricing;
+
+import java.util.concurrent.Callable;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.contrib.ev.EvConfigGroup;
+import org.matsim.contrib.ev.EvModule;
+import org.matsim.contrib.ev.routing.EvNetworkRoutingProvider;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
+import org.matsim.core.config.groups.PlansCalcRouteConfigGroup;
+import org.matsim.core.config.groups.StrategyConfigGroup;
+import org.matsim.core.controler.AbstractModule;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.OutputDirectoryHierarchy;
+import org.matsim.core.mobsim.qsim.AbstractQSimModule;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.vehicles.VehicleCapacity;
+import org.matsim.vehicles.VehicleType;
+import org.matsim.vehicles.Vehicles;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import urbanEV.UrbanEVConfigGroup;
+import urbanEV.UrbanEVModule;
+import urbanEV.UrbanVehicleChargingHandler;
+
+public final class RunEVExampleV2 implements Callable<Integer> {
+  public static final String COLOR = "@|bold,fg(81) ";
+   
+  private static final Logger log = LogManager.getLogger(RunEVExampleV2.class);
+  
+  @Option(names = {"--config"}, description = {"Optional Path to config file to load."}, defaultValue = "config.xml")
+  private String config;
+  
+  @Option(names = {"--plan"}, description = {"Optional Path to plan file to load."}, defaultValue = "prepared_plan.xml.gz")
+  private String planFile;
+  
+  @Option(names = {"--network"}, description = {"Optional Path to network file to load."}, defaultValue = "montreal_network.xml.gz")
+  private String networkFileLoc;
+  
+  @Option(names = {"--ts"}, description = {"Optional Path to transit schedule file to load."}, defaultValue = "montreal_transit_schedules.xml.gz")
+  private String tsFileLoc;
+  
+  @Option(names = {"--tv"}, description = {"Optional Path to transit vehicle file to load."}, defaultValue = "montreal_transit_vehicles.xml.gz")
+  private String tvFileLoc;
+  
+  @Option(names = {"--facilities"}, description = {"Optional Path to facilities file to load."}, defaultValue = "montreal_facilities.xml.gz")
+  private String facilitiesFileLoc;
+  
+  @Option(names = {"--lastiterations"}, description = {"Maximum number of iteration to simulate."}, defaultValue = "250")
+  private int maxIterations;
+  
+  @Option(names = {"--firstiterations"}, description = {"Maximum number of iteration to simulate."}, defaultValue = "0")
+  private int minIterations;
+  
+  @Option(names = {"--household"}, description = {"Optional Path to household file to load."}, defaultValue = "montreal_households.xml.gz")
+  private String householdFileLoc;
+  
+  @Option(names = {"--scale"}, description = {"Scale of simulation"}, defaultValue = "0.5")
+  private Double scale;
+  
+  @Option(names = {"--output"}, description = {"Result output directory"}, defaultValue = "output/")
+  private String output;
+  
+  @Option(names = {"--charger"}, description = {"Charger file location"}, defaultValue = "charger.xml")
+  private String chargerFile;
+  
+  @Option(names = {"--vehicles"}, description = {"Vehicles file"}, defaultValue = "vehicles.xml")
+  private String vehicleFile;
+  
+  @Option(names = {"--evehicle"}, description = {"Electric vehicle file"}, defaultValue = "evehicles.xml")
+  private String evehicleFile;
+  
+  @Option(names = {"--evpricing"}, description = {"Result output directory"}, defaultValue = "pricingProfiles.xml")
+  private String pricingEVFile;
+  
+  public static void main(String[] args) {
+    (new CommandLine(new RunEVExampleV2()))
+      .setStopAtUnmatched(false)
+      .setUnmatchedOptionsArePositionalParams(true)
+      .execute(args);
+  }
+  
+  public Integer call() throws Exception {
+    
+    Config config = ConfigUtils.loadConfig(this.config, new EvConfigGroup(), new UrbanEVConfigGroup());
+    
+    config.plans().setInputFile(this.planFile);
+    config.households().setInputFile(this.householdFileLoc);
+    config.facilities().setInputFile(this.facilitiesFileLoc);
+    config.network().setInputFile(this.networkFileLoc);
+    config.transit().setTransitScheduleFile(this.tsFileLoc);
+    config.transit().setVehiclesFile(this.tvFileLoc);
+    config.vehicles().setVehiclesFile(this.vehicleFile);
+    config.global().setNumberOfThreads(11);
+    config.qsim().setNumberOfThreads(10);
+    config.controler().setLastIteration(this.maxIterations);
+    config.controler().setFirstIteration(this.minIterations);
+    addStrategy(config, "SubtourModeChoice", null, 0.1D, 0 * this.maxIterations);
+    addStrategy(config, "ReRoute", null, 0.5D, 0 * this.maxIterations);
+    addStrategy(config, "ChangeExpBeta", null, 0.85D, this.maxIterations);
+    config.controler().setOutputDirectory(this.output);
+    config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
+    config.qsim().setFlowCapFactor(this.scale.doubleValue() * 1.2D);
+    config.qsim().setStorageCapFactor(this.scale.doubleValue() * 1.4D);
+    
+ 
+	((EvConfigGroup)config.getModules().get("ev")).setTimeProfiles(true);
+	((UrbanEVConfigGroup)config.getModules().get("urbanEV")).setPluginBeforeStartingThePlan(false);
+	((UrbanEVConfigGroup)config.getModules().get("urbanEV")).setMaxDistanceBetweenActAndCharger_m(500);
+	((UrbanEVConfigGroup)config.getModules().get("urbanEV")).setMaximumChargingProceduresPerAgent(2);
+	((UrbanEVConfigGroup)config.getModules().get("urbanEV")).setCriticalRelativeSOC(0.25);
+	((EvConfigGroup)config.getModules().get("ev")).setChargersFile(this.chargerFile);
+	((EvConfigGroup)config.getModules().get("ev")).setVehiclesFile(this.evehicleFile);
+	
+//    UrbanEVConfigGroup urbanEVConfigGroup = new UrbanEVConfigGroup();
+//    config.addModule(urbanEVConfigGroup);
+	
+    //config.controler().setLastIteration(100);
+	
+	config.controler()
+			.setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
+	config.qsim().setFlowCapFactor(0.1);
+	config.qsim().setStorageCapFactor(0.1);
+	config.global().setNumberOfThreads(14);
+	config.qsim().setNumberOfThreads(10);
+	//config.qsim().setVehiclesSource(VehiclesSource.defaultVehicle);
+	
+	
+	
+	config.planCalcScore().setPerforming_utils_hr(14);
+	
+//	UrbanEVConfigGroup evReplanningCfg = new UrbanEVConfigGroup(); // create the urbanEV config group
+//	
+//	config.addModule(evReplanningCfg);
+
+	//TODO actually, should also work with all AccessEgressTypes but we have to check (write JUnit test)
+	config.plansCalcRoute().setAccessEgressType(PlansCalcRouteConfigGroup.AccessEgressType.none);
+
+	//register charging interaction activities for car
+	config.planCalcScore().addActivityParams(
+			new PlanCalcScoreConfigGroup.ActivityParams(TransportMode.car + UrbanVehicleChargingHandler.PLUGOUT_INTERACTION)
+					.setScoringThisActivityAtAll(false));
+	config.planCalcScore().addActivityParams(
+			new PlanCalcScoreConfigGroup.ActivityParams(TransportMode.car + UrbanVehicleChargingHandler.PLUGIN_INTERACTION)
+					.setScoringThisActivityAtAll(false));
+
+	Scenario scenario = ScenarioUtils.loadScenario(config);
+	
+	
+	scaleDownPt(scenario.getTransitVehicles(), 0.1);
+	
+	
+	Controler controler = new Controler(scenario);
+	//controler.addOverridingModule(new EvModule());
+	controler.addOverridingModule(new UrbanEVModule());
+	//controler.addOverridingModule(new EVPriceModule());
+
+	
+	
+	
+	ChargerPricingProfiles pricingProfiles = new ChargerPricingProfileReader().readChargerPricingProfiles(this.pricingEVFile);
+
+	
+	controler.addOverridingModule(new AbstractModule() {
+		@Override
+		public void install() {
+			bind(ChargerPricingProfiles.class).toInstance(pricingProfiles);
+			addRoutingModuleBinding(TransportMode.car).toProvider(new EvNetworkRoutingProvider(TransportMode.car));
+			installQSimModule(new AbstractQSimModule() {
+				@Override
+				protected void configureQSim() {
+					//bind(VehicleChargingHandler.class).asEagerSingleton();
+					bind(ChargePricingEventHandler.class).asEagerSingleton();
+					//addMobsimScopeEventHandlerBinding().to(VehicleChargingHandler.class);
+					addMobsimScopeEventHandlerBinding().to(ChargePricingEventHandler.class);
+					this.addQSimComponentBinding(EvModule.EV_COMPONENT).to(ChargePricingEventHandler.class);
+				}
+				
+			});
+		}
+	});
+    
+    
+    
+	controler.configureQSimComponents(components -> components.addNamedComponent(EvModule.EV_COMPONENT));
+
+	controler.run();
+    return Integer.valueOf(0);
+  }
+  
+  public static void addStrategy(Config config, String strategy, String subpopulationName, double weight, int disableAfter) {
+    if (weight <= 0.0D || disableAfter < 0)
+      throw new IllegalArgumentException("The parameters can't be less than or equal to 0!"); 
+    StrategyConfigGroup.StrategySettings strategySettings = new StrategyConfigGroup.StrategySettings();
+    strategySettings.setStrategyName(strategy);
+    strategySettings.setSubpopulation(subpopulationName);
+    strategySettings.setWeight(weight);
+    if (disableAfter > 0)
+      strategySettings.setDisableAfter(disableAfter); 
+    config.strategy().addStrategySettings(strategySettings);
+  }
+  
+  public static void scaleDownPt(Vehicles transitVehicles, double portion) {
+		for(VehicleType vt: transitVehicles.getVehicleTypes().values()) {
+			VehicleCapacity vc = vt.getCapacity();
+			vc.setSeats((int) Math.ceil(vc.getSeats().intValue() * portion));
+			vc.setStandingRoom((int) Math.ceil(vc.getStandingRoom().intValue() * portion));
+			vt.setPcuEquivalents(vt.getPcuEquivalents() * portion);
+		}
+	}
+}
